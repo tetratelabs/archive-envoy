96b12a71bae59c5ff12b54d1a332aeb43d93bcc6
Dhi Aurrahman <dio@rockybars.com>
2023-10-18 10:31:53 +0700

    Scan all release pages
    This makes sure we scan all release pages, hence the result is more accurate and to avoid missing releases.
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- bin/consolidate_release_versions.sh
+++ bin/consolidate_release_versions.sh
@@ -22,6 +22,11 @@ curl --version >/dev/null
 jq --version >/dev/null
 curl="curl -fsSL"
 
+# A valid GitHub token to avoid rate limiting.
+githubToken=${GITHUB_TOKEN:-}
+# Prepare authorization header when performing request to api.github.com to avoid rate limiting, especially when testing locally.
+authorizationHeader="Authorization: Bearer ${githubToken}"
+
 githubRepo="${1?-githubRepo required ex tetratelabs/archive-envoy}"
 downloadBaseURL="${2?-downloadBaseURL required ex https://archive.tetratelabs.io/envoy/download}"
 case "${3:-0}" in
@@ -33,20 +38,30 @@ esac
 # This must match netlify.toml redirects
 redirectsTo="https://github.com/${githubRepo}/releases/download"
 
+# Fetch the last page number of releases (example value: 7), so we can get all of the releases.
+# To get the last page, we send a HEAD request to "https://api.github.com/repos/${githubRepo}/releases",
+# then "grep" the "link" header value.
+# Reference: https://docs.github.com/en/rest/guides/using-pagination-in-the-rest-api?apiVersion=2022-11-28#using-link-headers.
+lastReleasePage=$(${curl}I ${githubToken:+ -H "${authorizationHeader}"} "https://api.github.com/repos/${githubRepo}/releases" |
+  grep -Eo 'page=[0-9]+' | awk 'NR==2' | cut -d'=' -f2) || exit 1
+
 # archive all dists for the version, generating the envoy-versions.json format incrementally
 releaseVersions="{}"
-versions=$(${curl} "https://api.github.com/repos/${githubRepo}/releases?per_page=100" |
-  jq -er ".|map(select(.prerelease == false and .draft == false))|.[]|.name" | sort -n) || exit 1
 
-for version in ${versions}; do
-  # Exclusively handle debug
-  case ${version} in v[0-9]*[0-9]_debug) nextDebugVersion=1 ;; *) unset nextDebugVersion;; esac
-  [ "${debugVersion:-}" != "${nextDebugVersion:-}" ] && continue
+for ((page = 1; page <= lastReleasePage; page++)); do
+  versions=$(${curl} ${githubToken:+ -H "${authorizationHeader}"} "https://api.github.com/repos/${githubRepo}/releases?page=${page}" |
+    jq -er ".|map(select(.prerelease == false and .draft == false))|.[]|.name" | sort -n) || exit 1
 
-  versionsUrl="${redirectsTo}/${version}/envoy-${version}.json"
-  nextReleaseVersion=$(${curl} "${versionsUrl}" | sed "s~${redirectsTo}~${downloadBaseURL}~g") || exit 1
-  # merge the pending releaseVersions json to include the next one
-  releaseVersions=$(echo "${releaseVersions}" "${nextReleaseVersion}" | jq -Sse '.[0] * .[1]')
+  for version in ${versions}; do
+    # Exclusively handle debug.
+    case ${version} in v[0-9]*[0-9]_debug) nextDebugVersion=1 ;; *) unset nextDebugVersion;; esac
+    [ "${debugVersion:-}" != "${nextDebugVersion:-}" ] && continue
+
+    versionsUrl="${redirectsTo}/${version}/envoy-${version}.json"
+    nextReleaseVersion=$(${curl} "${versionsUrl}" | sed "s~${redirectsTo}~${downloadBaseURL}~g") || exit 1
+    # merge the pending releaseVersions json to include the next one.
+    releaseVersions=$(echo "${releaseVersions}" "${nextReleaseVersion}" | jq -Sse '.[0] * .[1]')
+  done
 done
 
 # reorder top-level keys so that versions appear before sha256sums
